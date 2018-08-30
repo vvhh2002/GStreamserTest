9daf928b083ce1e8ef71da07541454e535b5d6a8
Victor <drjone@gmail.com>
2018-08-30 16:30:40 +0800

    init

--- electron/app/index.js
+++ electron/app/index.js
@@ -1,11 +1,28 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
-function createWindow () {
+function createWindow() {
   // 创建浏览器窗口
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 1024, height: 768,
+    webPreferences: {
+      offscreen: true
+    }
+  });
 
   // 然后加载应用的 index.html。
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+  win.webContents.on('paint', (event, dirty, image) => {
+    // updateBitmap(dirty, image.getBitmap())
+    console.log(dirty);
+    console.log(image.getBitmap().length);
+  });
+  win.webContents.setFrameRate(60);
+
+
+  win.on('show', () => {
+    app.hide();
+  });
 }
 
-app.on('ready', createWindow)
+// app.disableHardwareAcceleration();
+app.on('ready', createWindow);
